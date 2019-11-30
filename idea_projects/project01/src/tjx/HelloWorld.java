4bdc17c4bd686cf8c8fa2f3f4717bff4a17fab66
BIGTANGBRO <992127134@qq.com>
2019-11-30 22:09:26 +0000

    First

---
+++ idea_projects/project01/src/tjx/HelloWorld.java
@@ -0,0 +1,8 @@
+package tjx;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is the first java program under linux");
+        System.out.println("wtf is this");
+    }
+}
