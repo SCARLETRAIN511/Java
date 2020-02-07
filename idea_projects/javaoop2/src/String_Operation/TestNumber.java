4715943c6488eba5299cf88dcc6800b047df0294
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-07 21:24:03 +0000

    1

--- idea_projects/javaoop2/src/String_Operation/TestNumber.java
+++ idea_projects/javaoop2/src/String_Operation/TestNumber.java
@@ -1,4 +1,12 @@
 package String_Operation;
 
 public class TestNumber {
+    public static void main(String[] args) {
+        int i = 5;
+        Integer it = new Integer(i);//转换为Integer 对象
+        int i2 = it.intValue();//转为int
+
+        Integer it2 = i2;//自动装箱
+        int i3 = it;//自动拆箱
+    }
 }
