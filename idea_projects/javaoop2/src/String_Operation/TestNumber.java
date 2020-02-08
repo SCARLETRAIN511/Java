4298941e8cf7c7d0afb604937c423fda843c47b2
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-08 00:18:31 +0000

    1

--- idea_projects/javaoop2/src/String_Operation/TestNumber.java
+++ idea_projects/javaoop2/src/String_Operation/TestNumber.java
@@ -8,5 +8,18 @@ public class TestNumber {
 
         Integer it2 = i2;//自动装箱
         int i3 = it;//自动拆箱
+
+        System.out.println(i3);
+
+        String str1 = String.valueOf(i);//两种方法数字转字符串
+        String str2 = it2.toString();
+
+        String str3 = "999999";
+        int iStr = Integer.parseInt(str3);//转换字符串为数字
+        System.out.println(iStr);
+        float iStr1 = (float) iStr;
+
+        System.out.format("%,8d people died in this accident %n",iStr);//格式化输出
+        System.out.printf("%,020.3f people died in this accident %n",iStr1);;
     }
 }
