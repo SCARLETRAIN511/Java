db06c284cd8403de0dbd9cbe9d8e83dfbd70df1c
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-09 22:21:13 +0000

    1

--- idea_projects/javaoop2/src/String_Operation/MyStringBuffer.java
+++ idea_projects/javaoop2/src/String_Operation/MyStringBuffer.java
@@ -9,8 +9,76 @@ public class MyStringBuffer implements IStringBuffer {
         value = new char[capacity];
     }
 
-    public void append(String str){
-
+    public MyStringBuffer(String str){
+        if (str != null){
+            value = str.toCharArray();
+        }
+        length = value.length;
+        if (capacity < value.length){
+            capacity = value.length*2;
+        }
     }
 
+
+    public void reverse(){
+        for (int i = 0;i<length/2;i++){
+            char temp = value[i];
+            value[i] = value[length-i-1];
+            value[length-i-1]=temp;
+        }
+    }
+
+    public void insert(int pos,String b){
+        if (pos<0)
+            return;
+        if (pos>length)
+            return;
+        if (null==b)
+            return;
+
+        while (length+b.length()>capacity){
+            capacity = (int) ((length+b.length())*1.5f);
+            char[] newValue = new char[capacity];
+            System.arraycopy(value,0,newValue,0,length);
+            value = newValue;
+        }
+        char[] cs = b.toCharArray();
+        System.arraycopy(value,pos,value,pos+cs.length,length-pos);
+        System.arraycopy(cs,0,value,pos,cs.length);
+        length = length + cs.length;
+    }
+    public int length(){
+        return length;
+    }
+
+    public void append(String str){
+        insert(length,str);
+    }
+
+    public void append(char c){
+        append(String.valueOf(c));
+    }
+
+    public void insert(int pos, char b){
+        insert(pos,String.valueOf(b));
+    }
+
+    public void delete(int start, int end) {
+        if (start < 0)
+            return;
+        if (start>length)
+            return;
+        if (end < 0)
+            return;
+        if (end > length)
+            return;
+        if (start >= end)
+            return;
+        System.arraycopy(value,end,value,start,length-end);
+        length-=end-start;
+    }
+
+    public void delete(int start){
+        delete(start,length);
+    }
 }
