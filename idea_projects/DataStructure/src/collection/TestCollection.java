e3f7b39d3d23fce61530859113c186d48e9c76b7
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-15 20:15:29 +0000

    1

--- idea_projects/DataStructure/src/collection/TestCollection.java
+++ idea_projects/DataStructure/src/collection/TestCollection.java
@@ -9,5 +9,16 @@ public class TestCollection {
         System.out.println(heros.size());
         heros.add(new Hero("Timo"));
         System.out.println(heros.toString());
+        System.out.println(heros.get(1));
+        System.out.println(heros.indexOf(new Hero("Timo")));
+
+        ArrayList h1 = new ArrayList();
+        create(h1);
+        System.out.println(h1.indexOf(new Hero("hero2")));
+    }
+    public static void create(ArrayList h1){
+        for (int i = 0; i < 5;i++){
+            h1.add(new Hero("hero" + i));
+        }
     }
 }
